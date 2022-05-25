addab42e7a5e7dede80cb731c39c391f06704b1c
Rohitsuresh1 <99383088+Rohitsuresh1@users.noreply.github.com>
2022-05-25 00:01:44 -0400

    added local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,38 +2,92 @@ var historyEl=document.querySelector(".history");
 var dayEl=document.querySelector(".daycontainer");
 var forecastEl=document.querySelector(".forecast");
 var btnEl=document.querySelector(".btn");
-var inputCity;
-var geoApi="http://api.openweathermap.org/geo/1.0/direct";
+var inputCity; var counter;
+var cityEl=document.createElement("h2");
+var geoApi="http://api.openweathermap.org/geo/1.0/direct?q=";
 var apiKey="c6fc4977c85a1eebd5a68d26a53b18d6";
-var weatherApi="api.openweathermap.org/data/2.5/forecast";
+var weatherApi="https://api.openweathermap.org/data/2.5/onecall?";
 
 
 var date=moment().format('l'); 
+
+var geoApiCall = function(){
+ fetch(geoApi+inputCity+"&appid="+apiKey).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                var lat=data[0].lat;
+                var long=data[0].lon;
+                cityEl.className="city";
+                console.log(data);
+                cityEl.textContent=data[0].name+" "+date;
+                localStorage.setItem(counter,data[0].name);
+                counter++;
+                dayEl.appendChild(cityEl);
+                currentApiCall(lat,long);
+            })
+        } else {
+            alert("Error:"+response.statusText);
+        }
+    })
+};
+
+// var weatherUrl = `${weatherApiRoot}onecall?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
+// fetch(weatherUrl)
+//   .then(function (res) {
+//     return res.json();
+//   })
+//   .then(function (data) {
+//     var temptext = document.getElementById("current-temp");
+//     temptext.innerHTML = data.current.temp - 273;
+//     var windtext = document.getElementById("current-wind");
+//     windtext.innerHTML = data.current.wind_speed;
+//     var humidtext = document.getElementById("current-humid");
+//     humidtext.innerHTML = data.current.humidity;
+//     var UVtext = document.getElementById("current-UV");
+//     UVtext.innerHTML = data.current.uvi;
+//     console.log(data);
+var currentApiCall=function(lat,long){
+    fetch(weatherApi+"lat="+lat+"&lon="+long+"&appid="+apiKey).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+            console.log(data);
+            var temp=data.current.temp - 273;
+            var wind=data.current.wind_speed;
+            var humidity=data.current.humidity;
+            var uV=data.current.uvi;
+            console.log(temp,wind,humidity,uV);
+            
+            })
+        } else {
+            alert("Error:"+response.statusText);
+        }
+    })
+};
 
 
 var getLocalStorage = function() {
     var storedEl=document.createElement("button");
     storedEl.className="historybtn";
-    storedEl.textContent="test";
-
-    //loop through the items and make the history visible with a line on top
-  //  if(localStorage){
-    // if there are ithems in history append <hr> element first
-  //  for(i=0;i<.length;i++){
-
-  //  };
-  //  }
-  historyEl.appendChild(storedEl);
+    if(localStorage){
+        counter=localStorage.length-1;
+        var lineEL=document.createElement("hr");
+        historyEl.appendChild(lineEL);
+        for(i=counter;i>=0;i--){
+            var cityHistory=localStorage.getItem(i);
+            var storeHistory=document.createElement("button");
+            storeHistory.className="historybtn";
+            console.log(i,cityHistory);
+            storeHistory.textContent=cityHistory;
+            console.log(storeHistory);
+            historyEl.appendChild(storeHistory);
+        };
+    }
 };
 
 btnEl.addEventListener('click',function(){
     inputCity=document.getElementById("searchCity").value;
-    console.log(inputCity);
-    var cityEl=document.createElement("h3");
-    cityEl.className="city";
-    cityEl.textContent=inputCity+" "+date;
-    dayEl.appendChild(cityEl);
-
+    geoApiCall();
+    getLocalStorage();
 });
 
 
@@ -41,4 +95,3 @@ btnEl.addEventListener('click',function(){
 
 
 
-getLocalStorage();
